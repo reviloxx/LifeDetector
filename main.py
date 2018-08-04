2a6b2135aaf1757b402a973f5868fed34ab9a36d
Oliver St <oliver.st@gmx.net>
2018-08-04 08:02:44 +0200

    finished preprocessing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from face_aligner.align_faces import align_faces
 from frame_extractor import extract_frames
 from video_renderer import render_video
 
-inputFile = 'fraud7.mov'
+inputFile = 'vid_paper.mp4'
 
 inputDir = 'video_in/'
 inputPath = inputDir + inputFile
